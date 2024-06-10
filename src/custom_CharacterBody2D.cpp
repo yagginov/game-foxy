9e564a771347781f6cbaeb57f8b170bf7fbe35d5
Bohdan Yagginov <yagginov@gmail.com>
2024-06-10 23:47:27 +0300

    added the ability to dodge

--- src/custom_CharacterBody2D.cpp
+++ src/custom_CharacterBody2D.cpp
@@ -12,14 +12,30 @@ void CustomCharacterBody2D::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_pixels_in_meter"), &CustomCharacterBody2D::get_pixels_in_meter);
 	ClassDB::bind_method(D_METHOD("set_pixels_in_meter", "p_pixels_in_meter"), &CustomCharacterBody2D::set_pixels_in_meter);
 	ClassDB::add_property("CustomCharacterBody2D", PropertyInfo(Variant::FLOAT, "pixels_in_meter"), "set_pixels_in_meter", "get_pixels_in_meter");
+
+	ClassDB::bind_method(D_METHOD("get_dodge_execution_time"), &CustomCharacterBody2D::get_dodge_execution_time);
+	ClassDB::bind_method(D_METHOD("set_dodge_execution_time", "p_dodge_execution_time"), &CustomCharacterBody2D::set_dodge_execution_time);
+	ClassDB::add_property("CustomCharacterBody2D", PropertyInfo(Variant::FLOAT, "dodge_execution_time"), "set_dodge_execution_time", "get_dodge_execution_time");
+
+	ClassDB::bind_method(D_METHOD("get_length_dodge_line"), &CustomCharacterBody2D::get_length_dodge_line);
+	ClassDB::bind_method(D_METHOD("set_length_dodge_line", "p_length_dodge_line"), &CustomCharacterBody2D::set_length_dodge_line);
+	ClassDB::add_property("CustomCharacterBody2D", PropertyInfo(Variant::FLOAT, "length_dodge_line"), "set_length_dodge_line", "get_length_dodge_line");
 }
 
 
 CustomCharacterBody2D::CustomCharacterBody2D() {
-	speed = 3.0; // 3 m/s
+	// Initialize any variables here.
+	speed = 2.0; // 2 m/s
 	pixels_in_meter = 32.0; // 32 pixels/m
 	i = Input::get_singleton();
-	// Initialize any variables here.
+
+	dodge_helper = Vector2(0.0, 0.0);
+	dodge_time = 0.0f;
+	in_dodge = false;
+
+	length_dodge_line = 2;
+	dodge_execution_time = 0.5;
+
 }
 
 CustomCharacterBody2D::~CustomCharacterBody2D() {
@@ -31,12 +47,45 @@ void CustomCharacterBody2D::_process(double delta) {
 }
 
 void CustomCharacterBody2D::_physics_process(double delta) {
+	
+	// Check dodge
+	if (dodge_method(delta)) { return; }
+
+	// Movement logic--------------
 	Vector2 input_direction = i->get_vector("ui_left", "ui_right", "ui_up", "ui_down");
 	set_velocity(input_direction * speed * pixels_in_meter);
-
 	move_and_slide();
 }
 
+
+bool CustomCharacterBody2D::dodge_method(double delta)
+{
+	// Dodge logic--------------
+	if (i->is_action_just_pressed("dodge"))
+	{
+		dodge_helper = i->get_vector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (dodge_helper.x == 0 && dodge_helper.y == 0)
+		{
+			return false;
+		}
+		in_dodge = true;
+	}
+	else if (!in_dodge)
+	{
+		return false;
+	}
+
+	set_velocity(dodge_helper * (length_dodge_line / dodge_execution_time) * pixels_in_meter);
+	move_and_slide();
+	dodge_time += delta;
+	
+	if (dodge_time > dodge_execution_time)
+	{
+		in_dodge = false;
+		dodge_time = 0.0;
+	}
+	return true;
+}
 
 void CustomCharacterBody2D::set_speed(const double p_speed) {
 	speed = p_speed;
@@ -52,4 +101,20 @@ void CustomCharacterBody2D::set_pixels_in_meter(const double p_pixels_in_meter) 
 
 double CustomCharacterBody2D::get_pixels_in_meter() const {
 	return pixels_in_meter;
+}
+
+void CustomCharacterBody2D::set_dodge_execution_time(const double p_dodge_execution_time) {
+	dodge_execution_time = p_dodge_execution_time;
+}
+
+double CustomCharacterBody2D::get_dodge_execution_time() const {
+	return dodge_execution_time;
+}
+
+void CustomCharacterBody2D::set_length_dodge_line(const double p_length_dodge_line) {
+	length_dodge_line = p_length_dodge_line;
+}
+
+double CustomCharacterBody2D::get_length_dodge_line() const {
+	return length_dodge_line;
 }
--- src/custom_CharacterBody2D.h
+++ src/custom_CharacterBody2D.h
@@ -11,10 +11,15 @@ class CustomCharacterBody2D : public CharacterBody2D {
 
 private:
 	Input* i;
+	Vector2 dodge_helper;
+	double dodge_time;
+	bool in_dodge;
 
 public:
-	double speed;   // meters in second
-	double pixels_in_meter; // count pixels in one meter
+	double speed;   // meters in second; default 2 m/s
+	double pixels_in_meter; // count pixels in one meter; default 32 px/m
+	double length_dodge_line; // default 2 meters
+	double dodge_execution_time; // default 0.5 seconds
 
 protected:
 	static void _bind_methods();
@@ -25,12 +30,20 @@ public:
 
 	void _process(double delta) override;
 	void _physics_process(double delta) override;
+
+	bool dodge_method(double delta);
 	
 	void set_speed(const double p_speed);
 	double get_speed() const;
 
 	void set_pixels_in_meter(const double p_pixels_in_meter);
 	double get_pixels_in_meter() const;
+
+	void set_length_dodge_line(const double p_length_dodge_line);
+	double get_length_dodge_line() const;
+
+	void set_dodge_execution_time(const double p_dodge_execution_time);
+	double get_dodge_execution_time() const;
 };
 
 }
