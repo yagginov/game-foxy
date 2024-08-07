fa30bfd62281cc4ec61942bdc877245f92781aed
Bohdan Yagginov <yagginov@gmail.com>
2024-08-07 13:10:44 +0300

    add methods

--- src/arrow.cpp
+++ src/arrow.cpp
@@ -13,7 +13,19 @@ void Arrow::_bind_methods()
 Arrow::Arrow() 
 {
 	// Initialize any variables here.	
-	
+	max_count_of_targets = 1;
+    count_of_targets = 0;
+    sprite_path = nullptr;
+    sprite = nullptr;
+
+    distance = 0;
+    max_distance = 32 * 8;
+    prev_position = Vector(0.0, 0.0);
+    direction = Vector2(0.0, 0.0);
+
+    v_states.push_back(new State(0.0));
+    v_states.push_back(new States(0.0));
+
 }
 
 Arrow::~Arrow() 
@@ -22,24 +34,83 @@ Arrow::~Arrow()
 
 }
 
-void Arrow::_ready() {
+void Arrow::_ready() 
+{
 	add_to_group("Arrow");
 
+
+}
+
+void Arrow::_physics_process(double delta) 
+{
+	switch(state)
+	{
+	case States::fly :
+		f_fly(delta);
+		break;
+    default:
+        if (v_states[state]->is_start())
+        {
+            turn_off();
+        } 
+        break;
+	}
+}
+
+void Arrow::f_fly(double delta)
+{
+    if (v_states[state].is_start())
+    {
+        turn_on();
+        prev_position = get_global_position();
+    }
+    
+    distance += (get_global_position() - prev_position).length(); 
+    
+    if (distance > max_distance)
+    {
+        reset();
+    }
 }
 
 void Arrow::hit(Vector2 target_pos)
 {
     Hitbox::hit(target_pos);
+    if (++count_of_targets > max_count_of_targets)
+    {
+        reset();
+    }
 }
 
 void Arrow::turn_on()
 {
     Hitbox::turn_on();
-    texture.set_visible(true);
+    if (sprite)
+    {
+        sprite.set_visible(true);
+    }
 }
 
 void Arrow::turn_off()
 {
     Hitbox::turn_off();
-    texture.set_visible(false);
+    if (sprite)
+    {
+        sprite.set_visible(false);
+    }
+}
+
+void Arrow::reset()
+{
+    v_states[state]->reset();
+    state = States::calm;
+    distance = 0.0;
+    count_of_targets = 0;
+}
+
+void Arrow::shot(const Vector2 p_direction)
+{
+    reset();
+    state = States::fly;
+    direction = p_direction;
 }
--- src/arrow.h
+++ src/arrow.h
@@ -2,8 +2,10 @@
 #define ARROW_H
 
 #include "components/hitbox.h"
+#include "state.h"
 #include <godot_cpp/classes/sprite2d.hpp>
-#include <godot_cpp/classes/node2d.hpp>
+#include <godot_cpp/variant/vector2.hpp>
+#include <godot_cpp/templates/vector.hpp>
 
 
 namespace godot {
@@ -16,6 +18,13 @@ protected:
 	static void _bind_methods();
 
 private:
+    enum States 
+    {
+        calm = 0, 
+        fly = 1
+    }
+
+private:
     // elements
     int                 max_count_of_targets;
     int                 count_of_targets;
@@ -23,19 +32,40 @@ private:
     NodePath            sprite_path;
     Sprite2D            sprite;
 
+    double              max_distance;
+    double              distance;
+    Vector2             prev_position;
+    Vector2             direction;
+
+    Vector<State*>       v_states;
+    States              state;
+
 public:
     Arrow();
 	~Arrow();
 
 	void _ready() override;
+    void _physics_process(double delta) override;
+
+    void f_fly(double delta);
 
     void hit(Vector2 target_pos) override;
 
     void turn_on() override;
     void turn_off() override;
 
+    void shot(const Vector2 p_direction);
+    void reset();
+
+    // set / get
     void set_sprite_path(const NodePath& p_sprite_path);
     NodePath get_sprite_path() const;
+
+    void set_max_count_of_targets(const int p_max_count_of_targets);
+    int get_max_count_of_targets() const;
+
+    void set_max_distance(const double p_max_distance);
+    double get_max_distance() const;
 
     
 }   // class Arrow
